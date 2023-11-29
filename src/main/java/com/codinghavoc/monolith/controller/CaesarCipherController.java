bcbe8877b1ad4e0ceb35e1b4b50c3024ba9e1996
sean <seanhayes13@hotmail.com>
2023-11-29 10:48:26 -0700

    ts

--- src/main/java/com/codinghavoc/monolith/controller/CaesarCipherController.java
+++ src/main/java/com/codinghavoc/monolith/controller/CaesarCipherController.java
@@ -2,6 +2,7 @@ package com.codinghavoc.monolith.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,11 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/caesarCipher")
 public class CaesarCipherController {
     CaesarCipherSvc ccSvc;
+
+    @GetMapping("/test")
+    public String sayHello(){
+        return "It works";
+    }
 
     @PostMapping("/process")
     public ResponseEntity<CipherBody> process(@RequestBody CipherBody cb){
