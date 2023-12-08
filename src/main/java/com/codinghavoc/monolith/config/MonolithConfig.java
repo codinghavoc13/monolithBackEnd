21355cb6d2a65bec1e380fee8ecfb938f4968c0c
sean <seanhayes13@hotmail.com>
2023-12-08 13:06:58 -0700

    202312081306 - added both https and http origins for frontend

--- src/main/java/com/codinghavoc/monolith/config/MonolithConfig.java
+++ src/main/java/com/codinghavoc/monolith/config/MonolithConfig.java
@@ -16,7 +16,7 @@ public class MonolithConfig {
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
                 // .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://monolithfrontend-795a0b654edf.herokuapp.com/")
+                .allowedOrigins("https://monolithfrontend-795a0b654edf.herokuapp.com/","http://monolithfrontend-795a0b654edf.herokuapp.com/")
                 .allowedMethods(HttpMethod.GET.name(),
                 HttpMethod.POST.name(),
                 HttpMethod.DELETE.name())
