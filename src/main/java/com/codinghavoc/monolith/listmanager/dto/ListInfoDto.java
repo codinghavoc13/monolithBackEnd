4db5422b03f62a8a65024947214ec441da4ea7fc
Sean Hayes <seanhayes13@hotmail.com>
2024-06-12 15:21:59 -0600

    202406121521

--- src/main/java/com/codinghavoc/monolith/listmanager/dto/ListInfoDto.java
+++ src/main/java/com/codinghavoc/monolith/listmanager/dto/ListInfoDto.java
@@ -7,6 +7,22 @@ import com.codinghavoc.monolith.listmanager.entity.ListInfo;
 
 public class ListInfoDto {
     public boolean ordered;
+    /*
+     * Look into creating sublists, lists within lists. Idea: a Gift
+     * list with sub lists for different people, or a shopping list with
+     * sublists for different stores.
+     * 
+     * Might want to enforce a limit of how many layers (main list-> 
+     * sub-list 1 -> sub-list 2 -> sub-list n) mostly for display purposes. 
+     * Each sub-list would be indented so from a mobile device POV would
+     * need to account for limited screen width.
+     * 
+     * Possible workaround would be to not display sub-lists but instead display
+     * buttons to view the sub-lists. Going with the workaround would allow 
+     * unlimited sub-lists. Liking this idea as a solution
+     * 
+     * Would also need to apply a parentListId to point to the upper list
+     */
     public List<ListItemDto> listItems;
     public Long listId;
     public Long numberOfItems;
